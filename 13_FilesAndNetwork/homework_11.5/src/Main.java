8f513a9caf773e6581d918a2390d06987ca81692
geoPinkman <geo.pimkin@gmail.com>
2022-01-10 18:10:26 +1000

    moscowMetroPArse

--- 13_FilesAndNetwork/homework_11.5/src/Main.java
+++ 13_FilesAndNetwork/homework_11.5/src/Main.java
@@ -3,26 +3,31 @@ import org.jsoup.nodes.Document;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        MetroLineList list = new MetroLineList();
+        MetroLineList lineList = new MetroLineList();
+        List<String> stationsList = new ArrayList<>();
         try {
             String htmlString = getHtml("data/MoscowMetro.html");
             Document document = Jsoup.parse(htmlString);
             System.out.println(document.title());
-            for(var qq : document.select("#metrodata > div > div.js-toggle-depend > span")) {
-                String qqString = qq.toString();
-                String[] test = qqString.split(" ");
-                list.addData(new MetroLine(test[5].replaceAll("[^0-9]", ""), test[6]));
+            for(var lines : document.select("#metrodata > div > div.js-toggle-depend > span")) {
+                String[] test = lines.toString().split(" ");
+                lineList.addData(new MetroLine(test[5].replaceAll("[^0-9]", ""), test[6]));
             }
-            for(var qqs : document.select("#metrodata > div > div:nth-child(4) > div")){
-                String test = qqs.toString();
-                System.out.println(test);
+            for(var stations : document.select("#metrodata > div > div > div")){
+                stationsList.add(stations.text());
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        List<MetroLine> test = lineList.getList();
+        for (int i = 0; i < test.size(); i++) {
+            System.out.println("Линия: " + test.get(i).getLineNumber() + " - "
+                    + test.get(i).getLineName() + " линия, Станции: " + stationsList.get(i));
         }
     }
     public static String getHtml(String path) {
---
+++ 13_FilesAndNetwork/homework_11.5/src/MetroStation.java
@@ -0,0 +1,12 @@
+public class MetroStation {
+    private int line;
+    private int stationNumber;
+    private String stationName;
+
+    public MetroStation(int line, int stationNumber, String stationName) {
+        this.line = line;
+        this.stationNumber = stationNumber;
+        this.stationName = stationName;
+    }
+
+}
