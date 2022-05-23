2c23403d439e37ee078687bbae16a0b9d3dfb1f0
geoPinkman <geo.pimkin@gmail.com>
2022-05-23 13:06:40 +1000

    remarks by testTask

--- flyAnalys/src/main/java/Main.java
+++ flyAnalys/src/main/java/Main.java
@@ -5,24 +5,34 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class Main {
+
+    public static final long CONVERT_TIME = 3600_000;
+
+    public static double getPercentile(List<Double> list, int percentile){
+        List<Double> res = list.stream().sorted(Double::compareTo).collect(Collectors.toList());
+        int index = percentile * res.size() / 100;
+        return res.get(index - 1);
+    }
+
     public static void main(String[] args) throws IOException {
         File file = new File("tickets.json");
 
         ObjectMapper mapper = new ObjectMapper();
         List<Double> fullTime = new ArrayList<>();
-        Map<Ticket, Double> ninetyPercentEachTime = new HashMap<>();
         mapper.readValue(file, Tickets.class).getTickets().forEach(line -> {
-            if (line.getOrigin().equals("VVO") & line.getDestination().equals("TLV")) {
+            if ((line.getOrigin().equals("VVO") & line.getDestination().equals("TLV")) |
+                    line.getOrigin().equals("TLV") & line.getDestination().equals("VVO")) {
                 String departureString = line.getDepartureDate() + " " + line.getDepartureTime();
                 String arrivalString = line.getArrivalDate() + " " + line.getArrivalTime();
                 try {
                     Date depData = new SimpleDateFormat("dd.MM.yy HH:mm").parse(departureString);
                     Date arrData = new SimpleDateFormat("dd.MM.yy HH:mm").parse(arrivalString);
-                    double time = (arrData.getTime() - depData.getTime()) / (1000 * 60 * 60 * 1.0);
+                    double time = (arrData.getTime() - depData.getTime()) / (CONVERT_TIME * 1.0);
                     fullTime.add(time);
-                    ninetyPercentEachTime.put(line, time * 0.9);
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
@@ -32,8 +42,12 @@ public class Main {
         for(double time : fullTime) {
             sumAllTimes += time;
         }
-        ninetyPercentEachTime.forEach((l,p) -> System.out.println(l.getOrigin() + " - " + l.getDestination() + " 90% полета это " + p + " часов"));
-        System.out.println(sumAllTimes / fullTime.size() + " часов в среднем за все полеты");
+        System.out.println("Среднее время в пути");
+        System.out.printf("%.2f" , sumAllTimes / fullTime.size());
+        System.out.println();
+        fullTime.stream().sorted(Double::compareTo).forEach(System.out::println);
+
+        System.out.println("90 - й процентиль : " + getPercentile(fullTime, 90));
     }
 
 }
